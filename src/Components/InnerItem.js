f02129847172b78de34e4299dcf9f30bcb8f7831
Harshit Gupta <149142309+harshitgupta240197@users.noreply.github.com>
2024-06-14 11:02:38 +0530

    first commit

---
+++ src/Components/InnerItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class InnerItem extends Component {
+
+  render() {
+    let {title, description, imageUrl, newsUrl} = this.props
+    return (
+      <div className='my-3'>
+        <div className="card" style={{width: "18rem"}}>
+            <img src={!imageUrl?"https://www.livemint.com/lm-img/img/2024/06/12/1600x900/stock_1718196461403_1718196461768.jpg":imageUrl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{title}...</h5>
+              <p className="card-text">{description}...</p>
+              <a rel='noreferrer' href={newsUrl} target='blank' className="btn btn-sm btn-dark">Read More</a>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default InnerItem
