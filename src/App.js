82616ad6ca5257e403ef444e04c1a338ee2c554f
Harshit Gupta <149142309+harshitgupta240197@users.noreply.github.com>
2024-06-16 03:11:28 +0530

    Added Infinite Scroll functionality to the website. Added notes on React Lifecycle.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
-
-//To import Class Based Component type RCE as shortcut to get a boilerplate code
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <News pageSize={6} country="in" category="science" />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<News key={"general"} pageSize={6} country="in" category="general" />} />
+            <Route exact path="/business" element={<News key={"business"} pageSize={6} country="in" category="business" />} />
+            <Route exact path="/entertainment" element={<News key={"entertainment"} pageSize={6} country="in" category="entertainment" />} />
+            <Route exact path="/health" element={<News key={"health"} pageSize={6} country="in" category="health" />} />
+            <Route exact path="/science" element={<News key={"science"} pageSize={6} country="in" category="science" />} />
+            <Route exact path="/sports" element={<News key={"sports"} pageSize={6} country="in" category="sports" />} />
+            <Route exact path="/technology" element={<News key={"technology"} pageSize={6} country="in" category="technology" />} />
+          </Routes>
+        </Router>
       </div>
     )
   }
 }
 
-export default App
+export default App;
--- src/Components/InnerItem.js
+++ src/Components/InnerItem.js
@@ -3,14 +3,18 @@ import React, { Component } from 'react'
 export class InnerItem extends Component {
 
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props
     return (
       <div className='my-3'>
         <div className="card">
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style = {{left: '90%', zIndex: '1'}}>
+          {source}
+        </span>
             <img src={!imageUrl?"https://www.livemint.com/lm-img/img/2024/06/12/1600x900/stock_1718196461403_1718196461768.jpg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{title}...</h5>
               <p className="card-text">{description}...</p>
+              <p className="card-text"><small className="text-body-secondary">By <strong>{!author?"unkown":author}</strong> on {new Date(date).toGMTString()}</small></p>
               <a rel='noreferrer' href={newsUrl} target='blank' className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
