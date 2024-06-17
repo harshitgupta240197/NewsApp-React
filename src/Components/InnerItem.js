eab1ded3d016de2ab50b36675d1d390feffba423
Harshit Gupta <149142309+harshitgupta240197@users.noreply.github.com>
2024-06-17 23:40:48 +0530

    Changed all the classs components except App.js to functional components.

--- src/Components/InnerItem.js
+++ src/Components/InnerItem.js
@@ -1,53 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class InnerItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } =
-      this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absolute",
-              right: "0",
-            }}
+const InnerItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span class="badge rounded-pill bg-danger">{source}</span>
+        </div>
+        <img
+          src={
+            !imageUrl
+              ? "https://www.livemint.com/lm-img/img/2024/06/12/1600x900/stock_1718196461403_1718196461768.jpg"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By <strong>{!author ? "unkown" : author}</strong> on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="blank"
+            className="btn btn-sm btn-dark"
           >
-            <span class="badge rounded-pill bg-danger">{source}</span>
-          </div>
-          <img
-            src={
-              !imageUrl
-                ? "https://www.livemint.com/lm-img/img/2024/06/12/1600x900/stock_1718196461403_1718196461768.jpg"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text">
-              <small className="text-body-secondary">
-                By <strong>{!author ? "unkown" : author}</strong> on{" "}
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a
-              rel="noreferrer"
-              href={newsUrl}
-              target="blank"
-              className="btn btn-sm btn-dark"
-            >
-              Read More
-            </a>
-          </div>
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default InnerItem;
